8a7f9947e15a086af2bb945ae443a3af31499904
Daniel Rodriguez <technoveltyco@users.noreply.github.com>
2023-02-09 20:58:14 +0000

    Add price currency exchange logic

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,90 +1,179 @@
 // JavaScript of Product listing page.
-const mainPageContentEl = document.querySelector('#main-page-content')
-const productListEl = document.querySelector('#product-list')
-const navSearchInputEl = document.querySelector('#nav-search-input')
-const navSearchButtonEl = document.querySelector('#nav-search-button')
-const navPrimaryItemsEl = document.querySelector('#nav-primary-items')
-const currencyDropdownEl = document.querySelector('#currency-dropdown')
+const mainPageContentEl = document.querySelector("#main-page-content");
+const productListEl = document.querySelector("#product-list");
+const navSearchInputEl = document.querySelector("#nav-search-input");
+const navSearchButtonEl = document.querySelector("#nav-search-button");
+const navPrimaryItemsEl = document.querySelector("#nav-primary-items");
+const currencyDropdownEl = document.querySelector("#currency-dropdown");
+const currencyDropdownButtonEl = document.querySelector(
+  "#currencyDropdownButton"
+);
+const currencyMenuItemsEl = document.querySelector("#currency-menu-items");
 
 const endpoints = {
   platzi: {
-    products: 'https://api.escuelajs.co/api/v1/products/',
-    categories: 'https://api.escuelajs.co/api/v1/categories?limit=5',
+    products: "https://api.escuelajs.co/api/v1/products/",
+    categories: "https://api.escuelajs.co/api/v1/categories?limit=5",
   },
   faker: {
-    credit_cards: 'https://fakerapi.it/api/v1/credit_cards?_quantity=1',
+    credit_cards: "https://fakerapi.it/api/v1/credit_cards?_quantity=1",
   },
   exchange_rate: {
     currency:
-      'https://v6.exchangerate-api.com/v6/dc9edcf7a653563d44d04acc/latest/GBP',
+      "https://v6.exchangerate-api.com/v6/dc9edcf7a653563d44d04acc/latest/GBP",
   },
-}
+};
+
+const currencies = {
+  CAD: {
+    countryName: "Canada",
+    currencyCode: "CAD",
+    currencySign: "c$",
+    icon: "./assets/img/Flag_of_Canada.svg",
+  },
+  EUR: {
+    countryName: "European Union",
+    currencyCode: "EUR",
+    currencySign: "€",
+    icon: "./assets/img/Flag_of_Europe.svg",
+  },
+  GBP: {
+    countryName: "United Kingdom",
+    currencyCode: "GBP",
+    currencySign: "£",
+    icon: "./assets/img/United-kingdom_flag_icon_round.svg",
+  },
+  USD: {
+    countryName: "United States",
+    currencyCode: "USD",
+    currencySign: "$",
+    icon: "./assets/img/United-states_flag_icon_round.svg",
+  },
+};
 
 // Request HTTP Method and Headers
 let options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
-}
+};
 
-let exchangeRates
-storeGBPExchangeRates()
+let exchangeRates;
+storeGBPExchangeRates();
 
 // DEV: for products page testing, add the tailwind 'hidden' class ('display: none')
-document.querySelector('#hero-banner').classList.add('hidden')
+document.querySelector("#hero-banner").classList.add("hidden");
 
 // add categories dynamically to main nav, from api
-populateNavPrimaryItems()
+populateNavPrimaryItems();
+
+let selectedCurrencyCode =
+  localStorage.getItem("selectedCurrencyCode") || "GBP";
+renderSelectedCurrency(selectedCurrencyCode);
+renderCurrencies();
 
 // FUNCTIONS ------------------------------------------------------- //
 
+function renderSelectedCurrency(currencyCode) {
+  const html = `
+  <img
+    src="${currencies[currencyCode].icon}"
+    class="rounded-full"
+    alt="${currencies[currencyCode].countryName} flag"
+    width="15px"
+    height="15px"
+    loading="lazy"
+  />&nbsp; ${currencies[currencyCode].currencySign} <i class="fa-solid fa-angle-down"></i>
+  <i class="fa-solid fa-angle-up"></i> 
+  `;
+
+  currencyDropdownButtonEl.innerHTML = html;
+}
+
+function renderCurrencies() {
+  let html = "";
+
+  Object.keys(currencies).forEach((currency) => {
+    if (currency !== selectedCurrencyCode) {
+      html += renderCurrencyItem(currency);
+    }
+  });
+
+  currencyMenuItemsEl.innerHTML = html;
+}
+
+function renderCurrencyItem(currencyCode) {
+  return `
+<li
+  role="menuitem"
+  class="hover:bg-blue-gray-50 focus:bg-blue-gray-50 active:bg-blue-gray-50 hover:text-blue-gray-900 focus:text-blue-gray-900 active:text-blue-gray-900 bg-blue-gray-50/80 text-blue-gray-900 block w-full cursor-pointer select-none rounded-md py-1.5 px-2.5 text-start text-xs font-medium transition-all hover:bg-opacity-80 focus:bg-opacity-80 active:bg-opacity-80"
+>
+  <a
+    href="#"
+    data-country-name="${currencies[currencyCode].countryName}"
+    data-currency-code="${currencies[currencyCode].currencyCode}"
+    data-currency-sign="${currencies[currencyCode].currencySign}"
+  >
+    <img
+      src="${currencies[currencyCode].icon}"
+      class="rounded-full"
+      alt="${currencies[currencyCode].name} flag"
+      width="25px"
+      height="25px"
+      loading="lazy"
+    />&nbsp; ${currencies[currencyCode].currencySign}
+  </a>
+</li>
+  `;
+}
+
 function storeGBPExchangeRates() {
   // only fetch from api if it doesn't exsist - to save limited api calls while testing
-  if (!localStorage.getItem('GBPExchangeRates')) {
-    alert('ExchangeRate API Call')
+  if (!localStorage.getItem("GBPExchangeRates")) {
+    alert("ExchangeRate API Call");
     // ExchangeRate API
-    fetch(endpoints['exchange_rate']['currency'], options)
+    fetch(endpoints["exchange_rate"]["currency"], options)
       .then((response) => response.json())
       .then((response) => {
-        console.log(response)
+        console.log(response);
 
-        exchangeRates = JSON.stringify(response)
-        localStorage.setItem('GBPExchangeRates', exchangeRates)
+        exchangeRates = JSON.stringify(response);
+        localStorage.setItem("GBPExchangeRates", exchangeRates);
       })
-      .catch((err) => console.error(err))
+      .catch((err) => console.error(err));
   } else {
-    exchangeRates = JSON.parse(localStorage.getItem('GBPExchangeRates'))
+    exchangeRates = JSON.parse(localStorage.getItem("GBPExchangeRates"));
   }
 }
 
 function fetchProductsEndpoint(queryString) {
-  fetch(endpoints['platzi']['products'] + queryString, options)
+  fetch(endpoints["platzi"]["products"] + queryString, options)
     .then((response) => response.json())
     .then((response) => {
-      console.log(response)
+      console.log(response);
 
       // clear contents before populating
-      productListEl.innerHTML = ''
+      productListEl.innerHTML = "";
 
       response.forEach((item) => {
-        createCard(item)
-      })
+        createCard(item);
+      });
     })
-    .catch((err) => console.error(err))
+    .catch((err) => console.error(err));
 }
 
 function populateNavPrimaryItems() {
   // clear existing list items
-  navPrimaryItemsEl.innerHTML = ''
+  navPrimaryItemsEl.innerHTML = "";
 
   // create category buttons from api
-  fetch(endpoints['platzi']['categories'], options)
+  fetch(endpoints["platzi"]["categories"], options)
     .then((response) => response.json())
     .then((response) => {
       response.forEach((category) => {
-        const newListItemEl = document.createElement('li')
-        newListItemEl.classList.add('nav-item')
+        const newListItemEl = document.createElement("li");
+        newListItemEl.classList.add("nav-item");
 
         newListItemEl.innerHTML = `
           <a
@@ -92,28 +181,34 @@ function populateNavPrimaryItems() {
             href="#!" data-category-id="${category.id}"
             >${category.name}</a
           >
-        `
-        navPrimaryItemsEl.appendChild(newListItemEl)
-      })
+        `;
+        navPrimaryItemsEl.appendChild(newListItemEl);
+      });
     })
-    .catch((err) => console.error(err))
+    .catch((err) => console.error(err));
 }
 
+function convertPrice(price) {
+  const currencyExchangeRate = Number.parseFloat(exchangeRates.conversion_rates[selectedCurrencyCode]);
+  return (price * currencyExchangeRate).toFixed(2);
+}
+
+
 function createCard(item) {
-  const newCardDiv = document.createElement('div')
+  const newCardDiv = document.createElement("div");
 
   newCardDiv.classList.add(
-    'w-full',
-    'm-1',
-    'max-w-sm',
-    'bg-white',
-    'border',
-    'border-gray-200',
-    'rounded-lg',
-    'shadow',
-    'dark:bg-gray-800',
-    'dark:border-gray-700'
-  )
+    "w-full",
+    "m-1",
+    "max-w-sm",
+    "bg-white",
+    "border",
+    "border-gray-200",
+    "rounded-lg",
+    "shadow",
+    "dark:bg-gray-800",
+    "dark:border-gray-700"
+  );
 
   newCardDiv.innerHTML = `
     <a href="#">
@@ -124,43 +219,54 @@ function createCard(item) {
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${item.title}</h5>
     </a>
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${item.description}</p>
-    <p class="mb-3 font-bold text-gray-900">£${item.price}</p>
+    <p class="mb-3 font-bold text-gray-900"><span class="currency">${currencies[selectedCurrencyCode].currencySign}</span><span class="price" data-original-price="${item.price}">${convertPrice(item.price)}</span></p>
     </div>
-  `
+  `;
 
-  productListEl.appendChild(newCardDiv)
+  productListEl.appendChild(newCardDiv);
 }
 
 // EVENT LISTNERS ------------------------------------------------------------ //
 
-navPrimaryItemsEl.addEventListener('click', (event) => {
-  event.preventDefault()
+navPrimaryItemsEl.addEventListener("click", (event) => {
+  event.preventDefault();
 
-  if (event.target.matches('a')) {
-    const categoryQuery = `?categoryId=${event.target.dataset.categoryId}`
-    fetchProductsEndpoint(categoryQuery)
+  if (event.target.matches("a")) {
+    const categoryQuery = `?categoryId=${event.target.dataset.categoryId}`;
+    fetchProductsEndpoint(categoryQuery);
   }
-})
+});
 
-navSearchButtonEl.addEventListener('click', (event) => {
-  const searchText = navSearchInputEl.value.trim()
+navSearchButtonEl.addEventListener("click", (event) => {
+  const searchText = navSearchInputEl.value.trim();
 
   if (searchText) {
-    const titleQuery = `?title=${searchText}`
-    fetchProductsEndpoint(titleQuery)
+    const titleQuery = `?title=${searchText}`;
+    fetchProductsEndpoint(titleQuery);
   }
-})
+});
 
-currencyDropdownEl.addEventListener('click', (event) => {
-  // const desiredCurrency = currencyDropdownEl.querySelector(':scope > li')
-  const desiredCurrency = event.target.parentNode.querySelector(':scope > a')
+currencyDropdownEl.addEventListener("click", (event) => {
+  event.preventDefault();
 
-  // // DEV: test exchangeRates
-  // console.log(exchangeRates)
+  if (event.target.parentNode.closest("ul")) {
+    let selectedEl = event.target.closest("li > a");
+    if (selectedEl) {
+      selectedCurrencyCode = selectedEl.dataset.currencyCode;
+      localStorage.setItem("selectedCurrencyCode", selectedCurrencyCode);
 
-  if (event.target.parentNode.closest('ul')) {
-    alert(desiredCurrency.dataset.countryName)
-    console.log(event.target.parentNode)
+      renderSelectedCurrency(selectedCurrencyCode);
+      renderCurrencies();
+
+      // Update product prices, if there is any.
+      document.querySelectorAll(".currency").forEach((currencyEl) => {
+        currencyEl.textContent = currencies[selectedCurrencyCode].currencySign;
+      });
+
+      document.querySelectorAll(".price").forEach((priceEl) => {
+        const productPrice = Number.parseFloat(priceEl.dataset.originalPrice);
+        priceEl.textContent = convertPrice(productPrice);
+      });
+    }
   }
-})
-
+});
